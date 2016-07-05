fac876d87206bbe259e04a479edd80b02b8ecc2c
604952096@qq.com <houkangzhu@hotmail.com>
2016-07-05 13:37:48 +0800

    Initial Commit

---
+++ KZOpenGLTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KZOpenGLTest
+//
+//  Created by HouKangzhu on 16/7/5.
+//  Copyright © 2016年 侯康柱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KZOpenGLTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KZOpenGLTest
+//
+//  Created by HouKangzhu on 16/7/5.
+//  Copyright © 2016年 侯康柱. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
